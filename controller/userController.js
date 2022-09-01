0c6ad21c84f7297ada645469c32f98947111d10c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-09-01 08:40:31 +0600

    User signup and signin with jwt token completed

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 const User = require("../model/userModel")
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 // SignIn Controller_____________________
@@ -13,16 +14,15 @@ module.exports.signUp = async (req, res) => {
     } else {
 
         const hash_password = await bcrypt.hash(password, 10)
-        const newUser = new User({ name, email, hash_password, picture, paymentId, reviewId })
+        const newUser = new User({ name, email, hash_password, picture, paymentId, reviewId, role: "user" })
 
         try {
-            const result = await newUser.save()
-            res.status(200).json({ message: "User Signed Up successfully.", result })
+            const user = await newUser.save()
+            res.status(200).json({ message: "User Signed Up successfully.", user })
         } catch (error) {
             res.status(500).json({ error })
         }
     }
-
 
 }
 
@@ -30,10 +30,23 @@ module.exports.signUp = async (req, res) => {
 module.exports.signIn = async (req, res) => {
 
     const { email, password } = req.body
-
+    const user = await User.findOne({ email })
 
     try {
-        res.status(200).json({})
+        if (user) {
+            const passwordCorrect = await bcrypt.compare(password, user.hash_password)
+            const { _id, role } = user
+            if (passwordCorrect) {
+                const token = jwt.sign({ _id, role }, process.env.JWT_SECRET)
+                res.status(200).json({ message: "User SignIn Successfully.", token, user })
+            } else {
+                res.status(500).json({ error: "Authentication Failed!" })
+            }
+
+        } else {
+            res.status(500).json({ error: "User Not found!" })
+        }
+
     } catch (error) {
         res.status(500).json({ error })
     }
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 const app = express()
+
+dotenv.config()
 
 // Local Middlewares
 const userRoute = require("./route/userRoute")
@@ -11,11 +14,11 @@ const adminRoute = require("./route/adminRoute")
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded())
 
+// DB Confiq
+require("./db/dbConfig")
+
 // Local Middlewares
 app.use("/api/user", userRoute)
 app.use("/api/admin", adminRoute)
-
-// DB Confiq
-require("./db/dbConfig")
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
