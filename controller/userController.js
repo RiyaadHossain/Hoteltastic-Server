32b8a7704b1710906f69efd80542ebfa6bf2613a
khanshorif331 <khanshorif331@gmail.com>
2022-09-21 20:27:31 +0600

    changed

--- controller/userController.js
+++ controller/userController.js
@@ -91,13 +91,3 @@ module.exports.getUser = async (req, res) => {
 		res.status(400).json({ error: error.message })
 	}
 }
-
-// Get All User Controller_____________________
-module.exports.getAllUser = async (req, res) => {
-	try {
-		const users = await User.find({})
-		res.status(200).json({ users })
-	} catch (error) {
-		res.status(400).json({ error: error.message })
-	}
-}
