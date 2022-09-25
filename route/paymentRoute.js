3dd686dad10ed6f4155f821203472690beb29025
khanshorif331 <khanshorif331@gmail.com>
2022-09-25 22:52:07 +0600

    updated bugs fixxing

--- route/paymentRoute.js
+++ route/paymentRoute.js
@@ -3,7 +3,6 @@ const { authenticateUser } = require('../middleware/auth')
 const paymentController = require('../controller/paymentController')
 
 const route = express.Router()
-console.log(paymentController.payment)
 // authenticateUser,
 route.post('/', paymentController.payment)
 
